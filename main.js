121a62ea7c563a200ff52a6e4eeb0bbcfabdc80d
JosiasAssamoi <assamoi.josias@gmail.com>
2019-12-05 12:53:55 +0100

    changing road ..

--- main.js
+++ main.js
@@ -2,12 +2,16 @@
 // et tu mets dedans 
 const express = require('./my-express')
 const app =  express()
+const url = require('url')
 
 app.get('/', function (req, res) {
-    res.write('hello world')
+  const {query} = url.parse(req.url,true)
+  const {name} = query
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write(`<h1>Hello ${name || 'World' }</h1>`)
   })
 
-  app.get('/hello', function (req, res) {
+app.get('/hello', function (req, res) {
       
     res.write('hello')
 
--- my-express.js
+++ my-express.js
@@ -4,31 +4,36 @@ const fs = require('fs')
 
 class myExpress{
     constructor(){
+
+        this.routes={}
         this.app = this.init()
         this.listen(8080)
+
+
     }
     init(){
-        const server = http.createServer(function (req, res) {
+        const server = http.createServer((req, res) => {
+            const {pathname} = url.parse(req.url,true)
+            if(this.routes[pathname+'-'+req.method]){
+                this.routes[pathname+'-'+req.method](req,res)
+            }
+            else{
+                res.write("ERROR PAGE NOT FOUND")
+            }
+            res.end()            
           })
           return server
     }
     get(path,cb){
-        this.app.on('request', (req,res) =>{
-            const {pathname, query} = url.parse(req.url,true)
-            console.log(pathname,path);
-            
-            if (pathname === path){
-                cb(req,res)
-                res.end()
-            }
-    
-        } )
+    //console.log('path =>',path)
+    this.routes[path+'-'+'GET']=cb
+    //console.log(this.routes)
     }
-    post(path,cb){
+    /*post(path,cb){
         this.app.on('request', (req,res) =>{
             cb(req,res)
         } )
-    }
+    }*/
     put(){
     } 
     delete(){
