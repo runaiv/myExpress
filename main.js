9a53282f590a92aad0778ec5e780d50f6baf0a2c
JosiasAssamoi <assamoi.josias@gmail.com>
2019-12-06 21:21:28 +0100

    handling basics method

--- main.js
+++ main.js
@@ -3,12 +3,15 @@
 const express = require('./my-express')
 const app =  express()
 const url = require('url')
+const fs = require('fs')
+const Route = require('route-parser')
+const LOCAL_DB= 'students.json'
 
 app.get('/', function (req, res) {
+
   const {query} = url.parse(req.url,true)
   const {name} = query
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  res.write(`<h1>Hello ${name || 'World' }</h1>`)
+  res.send(`<h1>Hello ${name || 'World' }</h1>`)
   })
 
 app.get('/hello', function (req, res) {
@@ -16,4 +19,81 @@ app.get('/hello', function (req, res) {
     res.write('hello')
 
   })
+
+  app.post('/students', function (req, res) {
+    data =""
+    req.on('data', chunk => {
+        data +=chunk.toString()
+    })
+    req.on("end", ()=>{
+        if (data){
+          console.log("data =>",data)
+          user=JSON.parse(data) // transform to  6
+          if(fs.existsSync(LOCAL_DB)){
+              const json = require(`./${LOCAL_DB}`)
+              // gere le cas ou on supprime un user entre 2 id afin deviter doublons
+              const max_id = Math.max.apply(Math, json.map(obj => obj.id))                
+              user.id = max_id +1 
+              json.push(user)
+              data = json
+          }
+          else{
+          user.id = 1 
+          data = [user]
+        }
+        fs.writeFileSync(LOCAL_DB,JSON.stringify(data,null,' '))
+        }     
+    })
+  })
+
+  app.put('/students/:id', function (req, res) {
+    if(fs.existsSync(LOCAL_DB)){
+      const json = require(`./${LOCAL_DB}`) 
+      const newJson = []
+      let data = ''
+      req.on('data', chunk => {
+          data +=chunk.toString()
+      })
+      req.on("end", ()=>{
+          if(data){
+            properties = JSON.parse(data)
+            for (user of json ) {
+              if(user.id==req.id){
+                Object.keys(properties).forEach(key => {
+                  value = properties[key]
+                  user[key]=value
+                })  
+              }
+              newJson.push(user)
+            } 
+            fs.writeFileSync(LOCAL_DB,JSON.stringify(newJson,null,' '))
+          }
+         
+      })
+    }
+
+  })
+
+  app.delete('/students/',function (req, res){
+    if(fs.existsSync(LOCAL_DB)){
+
+      const json = require(`./${LOCAL_DB}`) 
+      let newJson=[]
+      console.log('req ',req.id)
+      if (!req.id){
+        fs.writeFileSync(LOCAL_DB,JSON.stringify([],null,' '))
+      }
+      else{
+        for(user of json)
+          if(user.id != req.id){
+            newJson.push(user)
+          }
+        fs.writeFileSync(LOCAL_DB,JSON.stringify(newJson,null,' '))
+      }
+    }
+  })
+
+  app.all()
+
+  app.listen(8080)
 
--- my-express.js
+++ my-express.js
@@ -1,20 +1,35 @@
 let http = require('http')
 const url = require('url')
 const fs = require('fs')
+const Route = require('route-parser')
+
+function getiD(pathname){
+    return pathname[pathname.length-1] 
+}
 
 class myExpress{
     constructor(){
 
         this.routes={}
         this.app = this.init()
-        this.listen(8080)
-
-
+       // this.listen(8080)
     }
     init(){
         const server = http.createServer((req, res) => {
-            const {pathname} = url.parse(req.url,true)
-            if(this.routes[pathname+'-'+req.method]){
+            let {pathname} = url.parse(req.url,true)
+
+            if(req.method =="PUT" && /^\/students\/\d+$/.test(pathname) ){
+                req.id = getiD(pathname.split("/"))
+                this.routes[pathname+'-'+req.method](req,res)
+            }
+            else if (req.method =="DELETE" && /^\/students\/\d?$/.test(pathname)  ){
+                req.id = getiD(pathname.split("/"))
+                if (req.id){
+                    pathname = pathname.slice(0,-1) 
+                }
+                this.routes[pathname+'-'+req.method](req,res)   
+            }
+            else if(this.routes[pathname+'-'+req.method]){
                 this.routes[pathname+'-'+req.method](req,res)
             }
             else{
@@ -24,24 +39,26 @@ class myExpress{
           })
           return server
     }
-    get(path,cb){
-    //console.log('path =>',path)
-    this.routes[path+'-'+'GET']=cb
-    //console.log(this.routes)
+    send(message){
+        res.write(message)
     }
-    /*post(path,cb){
-        this.app.on('request', (req,res) =>{
-            cb(req,res)
-        } )
-    }*/
-    put(){
+    get(path,cb){
+        this.routes[path+'-'+'GET']=cb
+    }
+    post(path,cb){
+        this.routes[path+'-'+'POST']=cb
+    }
+    put(path,cb){
+         this.routes[path+"-"+"PUT"]=cb
     } 
-    delete(){
+    delete(path, cb){
+        this.routes[path+"-"+"DELETE"]=cb
     }
     all(){
+
     }
     listen(port){
-        this.app.listen(8080)
+        this.app.listen(port)
     }
     render(){
     }
