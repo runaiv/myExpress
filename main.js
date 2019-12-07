bfb166db45fe2e82ecb19159f5ad8b2729d8a6be
JosiasAssamoi <assamoi.josias@gmail.com>
2019-12-08 00:20:02 +0100

    render handling basics

--- main.js
+++ main.js
@@ -8,32 +8,45 @@ const Route = require('route-parser')
 const LOCAL_DB= 'students.json'
 
 app.get('/', function (req, res) {
+  console.log("GET FUNC CALLED...")
 
   const {query} = url.parse(req.url,true)
   const {name} = query
+  //res.writeHead(200, { 'Content-Type': 'text/html' })
   res.write(`<h1>Hello ${name || 'World' }</h1>`)  
   })
 
 app.get('/hello', function (req, res) {
       
-    res.write('hello')
+  app.render('home',{name:'Josias'}, (err, html) => {
+    if(err){
+      res.write(err)
+    }
+    else{
+      res.write(html)
+    }
+  
+  })
 
   })
 
   app.post('/students', function (req, res) {
+    console.log("POST FUNC CALLED....")
     data =""
     req.on('data', chunk => {
         data +=chunk.toString()
     })
     req.on("end", ()=>{
         if (data){
-          console.log("data =>",data)
+          //console.log("data =>",data)
           user=JSON.parse(data) // transform to  6
           if(fs.existsSync(LOCAL_DB)){
               const json = require(`./${LOCAL_DB}`)
               
               // gere le cas ou on supprime un user entre 2 id afin deviter doublons et le cas ou le fichier a est un tableau vide
-              const  max_id = json.lenght ==0 ? 0 : Math.max.apply(Math, json.map(obj => obj.id))          
+              const  max_id = Object.entries(json).length === 0 ? 0 : Math.max.apply(Math, json.map(obj => obj.id))   
+             // console.log(json,Object.entries(json).length === 0)
+                     
               user.id = max_id +1 
               json.push(user)
               data = json
@@ -43,11 +56,13 @@ app.get('/hello', function (req, res) {
           data = [user]
         }
         fs.writeFileSync(LOCAL_DB,JSON.stringify(data,null,' '))
+        
         }     
     })
   })
 
   app.put('/students/:id', function (req, res) {
+    console.log("PUT FUNC CALLED...")
     if(fs.existsSync(LOCAL_DB)){
       const json = require(`./${LOCAL_DB}`) 
       const newJson = []
@@ -76,12 +91,14 @@ app.get('/hello', function (req, res) {
   })
 
   app.delete('/students/:id',function (req, res){
+    console.log("DELETE FUNC CALLED...")
+    
     if(fs.existsSync(LOCAL_DB)){
 
       const json = require(`./${LOCAL_DB}`) 
       let newJson=[]
-     // console.log('req ',req.id)
       if (!req.id){
+        
         fs.writeFileSync(LOCAL_DB,JSON.stringify([],null,' '))
       }
       else{
@@ -95,13 +112,15 @@ app.get('/hello', function (req, res) {
   })
 
   app.all('/students/all', function (req,res){
-    //console.log(res.routes) 
     res.routes.forEach(([index,cb]) =>{
+      
       cb(req,res)
       
     });
     
   })
 
-  app.listen(8080)
+ 
+
+  app.listen(8080) 
 
--- my-express.js
+++ my-express.js
@@ -15,51 +15,57 @@ class myExpress{
        // this.listen(8080)
     }
     init(){
+        
         const server = http.createServer((req, res) => {
-            let {pathname} = url.parse(req.url,true)
+            let {pathname} = url.parse(req.url,true) 
+            res.setHeader('Content-Type','text/html') 
             pathname= /\w/.test(pathname) ? pathname.replace(/\/$/,"") : pathname // enleve le dernier / sauf pour la page d'accueil /
-            if(req.method =="PUT" && /^\/students\/\d+$/.test(pathname) ){
-                req.id = getiD(pathname.split("/"))
-                this.routes[pathname.replace(req.id,":id")+'-'+req.method](req,res)
-            }
-            else if (req.method =="DELETE" && /^\/students\/\d?$/.test(pathname) || req.method =="DELETE" && /^\/students$/.test(pathname) ){ 
-                if(pathname.endsWith('students')){
-                    pathname = pathname.replace('students','students/:id') 
+                if(req.method =="PUT" && /^\/students\/\d+$/.test(pathname) ){
+                    req.id = getiD(pathname.split("/"))
+                    this.routes[pathname.replace(req.id,":id")+'-'+req.method](req,res)
+                }
+                else if (req.method =="DELETE" && /^\/students\/\d?$/.test(pathname) || req.method =="DELETE" && /^\/students$/.test(pathname) ){ 
+                    if(pathname.endsWith('students')){
+                        pathname = pathname.replace('students','students/:id') 
+                    }
+                    else{
+                        req.id = getiD(pathname.split("/"))
+                        pathname = pathname.replace(req.id,':id')
+                    } 
+                    this.routes[pathname+'-'+req.method](req,res)   
+                }
+            
+                
+                else if(/^\/students\/all\/\d?$/.test(pathname) ||/^\/students\/all$/.test(pathname) ){
+                    //console.log(pathname);
+                    if(!pathname.endsWith('students/all')){
+                        req.id = getiD(pathname.split("/"))
+                        pathname = pathname.replace('/'+req.id,'')
+
+                    }
+                    res.routes = []
+                    // on va recuperer tte les callbacks
+                    Object.entries(this.routes).forEach(elt => {
+                        if (!/-ALL/.test(elt[0]))
+                            res.routes.push(elt) 
+                    })
+                
+                    this.routes[pathname+'-ALL'](req,res)
+                }
+                //GET ,POST, , ...
+                else if(this.routes[pathname+'-'+req.method]){
+                    this.routes[pathname+'-'+req.method](req,res)
+                    //resolve(res)
                 }
                 else{
-                    req.id = getiD(pathname.split("/"))
-                    pathname = pathname.replace(req.id,':id')
-                } 
-
-                this.routes[pathname+'-'+req.method](req,res)   
-            }
-          
-            
-            else if(/^\/students\/all\/\d?$/.test(pathname) ||/^\/students\/all$/.test(pathname) ){
-                //console.log(pathname);
-                if(!pathname.endsWith('students/all')){
-                    req.id = getiD(pathname.split("/"))
-                    pathname = pathname.replace('/'+req.id,'')
-
+                    res.write("ERROR PAGE NOT FOUND") 
                 }
-                res.routes = []
-                // on va recuperer tte les callbacks
-                Object.entries(this.routes).forEach(elt => {
-                    if (!/-ALL/.test(elt[0]))
-                        res.routes.push(elt) 
-                })
-               
-                this.routes[pathname+'-ALL'](req,res)
-            }
-            //GET ,POST, , ...
-            else if(this.routes[pathname+'-'+req.method]){
-                this.routes[pathname+'-'+req.method](req,res)
-            }
-            else{
-                res.write("ERROR PAGE NOT FOUND") 
-            }
-            res.end()            
-          })
+                
+                res.end()
+                  
+            })
+
+                       
           return server
     }
     send(message){
@@ -83,8 +89,26 @@ class myExpress{
     listen(port){
         this.app.listen(port)
     }
-    render(){
+    render(page, params=null,cb){
+        if (page == 'home'){
+            let content='' 
+            let err=''
+            try {
+                content = fs.readFileSync('html.mustache').toString()
+            }
+            catch (e){
+                err = e
+            }
+            
+            if (params){
+                Object.entries(params).forEach(param => {
+                    content=content.replace('{{'+param[0]+'}}',param[1]) 
+                })
+            }
+            cb(err,content)
+        } 
     }
+    
 }
 function express(){
     return new myExpress()
